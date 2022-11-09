9d28a1493c0415a4e8efebd08efabc01e6b1627d
Jakub Cizek <jcizekk@gmail.com>
2022-11-09 19:25:07 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/blocks-lockers/Bluetooth",
-            .branch("swift-5.5")
+            .branch("swift-5.6")
         )
     ],
     targets: [
